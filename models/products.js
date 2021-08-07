f381cdf4efdb87496aa1d6b9edae85a8a2d65d36
Ahsan Zulfiqar <mianahsan719@gmail.com>
2021-08-07 23:19:37 +0500

    vcv

--- models/products.js
+++ models/products.js
@@ -3,26 +3,26 @@ var uniqueValidator = require("mongoose-unique-validator");
 
 var productSchema = new mongoose.Schema(
   {
-    type: { type: String, lowercase: true, required: false }, // Men || Women
+    // type: { type: String, lowercase: true, required: false }, // Men || Women
     title: { type: String, required: false },
     description: { type: String, lowercase: true, required: false },
     category_id: { type: String, required: false },
     brand: { type: String, required: false },
-    collections: [], //  new products, tranding , on sale
+    // collections: [], //  new products, tranding , on sale
     sale: { type: Boolean, required: false, default: false },
     new: { type: Boolean, required: false },
     tags: [],
-    Watch_Case_Shape: { type: String },
-    Watch_Case_Size: { type: String },
-    Glass: { type: String },
-    Watch_Feature: [],
-    Model: { type: String },
-    Dial_Size: { type: String },
-    Movement: { type: String },
-    Watch_Movement_Country: { type: String },
-    Strap_Material: { type: String },
-    water_resistance: { type: Boolean },
-    Color_Family: [],
+    // Watch_Case_Shape: { type: String },
+    // Watch_Case_Size: { type: String },
+    // Glass: { type: String },
+    // Watch_Feature: [],
+    // Model: { type: String },
+    // Dial_Size: { type: String },
+    // Movement: { type: String },
+    // Watch_Movement_Country: { type: String },
+    // Strap_Material: { type: String },
+    // water_resistance: { type: Boolean },
+    // Color_Family: [],
     Warranty: {
       isWarranty: {
         type: Boolean,
